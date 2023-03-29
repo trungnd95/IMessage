b6845a7d0ab28ba04864a331a8bcf467061b3316
Trung Ng <trungnd.uet@gmail.com>
2023-03-30 00:19:10 +0700

    Frontend: add chakra ui

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,7 +3,6 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
   const { data } = useSession();
-  console.log(process.env.GOOGLE_CLIENT_ID);
   return (
     <>
       {data?.user ? (<button onClick={() => signOut()}>Signout</button>) : (<button onClick={() => signIn("google")} >Signin</button>)}
