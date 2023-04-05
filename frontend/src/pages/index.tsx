f0972502fa2b2e8a1cd11c32d21f83d6c8d16bac
Trung-Ng <trungnd233@viettel.com.vn>
2023-04-05 10:50:27 +0700

    frontend: add ssr to test useSession

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,26 @@
-import { NextPage } from "next";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { NextPage, NextPageContext } from 'next';
+import { getSession, signIn, signOut, useSession } from 'next-auth/react';
 
 const Home: NextPage = () => {
   const { data } = useSession();
   return (
     <>
-      {data?.user ? (<button onClick={() => signOut()}>Signout</button>) : (<button onClick={() => signIn("google")} >Signin</button>)}
+      {data?.user ? (
+        <button onClick={() => signOut()}>Signout</button>
+      ) : (
+        <button onClick={() => signIn('google')}>Signin</button>
+      )}
     </>
-  )
+  );
+};
+
+export async function getServerSideProps(ctx: NextPageContext) {
+  const session = await getSession(ctx);
+  return {
+    props: {
+      session,
+    },
+  };
 }
 
 export default Home;
