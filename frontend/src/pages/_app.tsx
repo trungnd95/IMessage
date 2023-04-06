1e6aaed396c5440c1d51ee3cc43246872dabeef4
Trung-Ng <trungnd233@viettel.com.vn>
2023-04-06 16:45:33 +0700

    frontend: add apollo graphql client

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,15 +1,22 @@
-import { theme } from '@/chakra/theme'
-import { ChakraProvider } from '@chakra-ui/react'
-import { SessionProvider } from 'next-auth/react'
-import type { AppProps } from 'next/app'
+import { theme } from '@/chakra/theme';
+import { useApollo } from '@/lib/apolloClient';
+import { ApolloProvider } from '@apollo/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  const apolloClient = useApollo(pageProps);
   return (
-    <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SessionProvider>
-  )
-
+    <ApolloProvider client={apolloClient}>
+      <SessionProvider session={session}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionProvider>
+    </ApolloProvider>
+  );
 }
