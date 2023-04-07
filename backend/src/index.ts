0ec4af82f432d947c4f8312ae29fc25b99c80682
Trung Nguyen <trungnd0808@gmail.com>
2023-04-07 04:02:48 -0700

    backend: install apollo server dependencies and construct server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,42 @@
-console.log("Hello world");
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { json } from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import http from 'http';
+import 'reflect-metadata';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/Hello';
+
+const main = async () => {
+  // Express server
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  // Apollo server
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      emitSchemaFile: true,
+      validate: false,
+    }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await apolloServer.start();
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>(),
+    json(),
+    expressMiddleware(apolloServer, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    }),
+  );
+
+  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+};
+
+main().catch((err) => {
+  console.log(err);
+});
